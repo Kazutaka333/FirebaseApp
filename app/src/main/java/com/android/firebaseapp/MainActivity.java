ea4854a4c75cffd043691797eac3fbfca427ba41
Kazutaka Homma <special18fender@gmail.com>
2018-02-26 12:34:39 -0800

    add firebase integration

--- app/src/main/java/com/android/firebaseapp/MainActivity.java
+++ app/src/main/java/com/android/firebaseapp/MainActivity.java
@@ -6,13 +6,24 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseDatabase database;
+    DatabaseReference myFirebaseRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,14 +31,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        database = FirebaseDatabase.getInstance();
+        myFirebaseRef = database.getReference();
     }
 
     @Override
@@ -56,6 +61,24 @@ public class MainActivity extends AppCompatActivity {
     {
         EditText etId = (EditText) findViewById(R.id.EditTextIdSearch);
         String idToSearch = etId.getText().toString();
+
+        Query queryRef = myFirebaseRef.orderByChild("studentId").equalTo(idToSearch);
+
+        queryRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot == null || dataSnapshot.getValue() == null)
+                    Toast.makeText(MainActivity.this, "No record found", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, dataSnapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("TAG1", "Failed to read value.", databaseError.toException());
+            }
+        });
+
     }
 
     public void submit(View button) {
@@ -68,6 +91,8 @@ public class MainActivity extends AppCompatActivity {
         Student student = new Student();
         student.setName(name);
         student.setStudentId(id);
+
+        myFirebaseRef.child(student.getStudentId()).setValue(student);
 
         Intent output = new Intent();
         setResult(RESULT_OK, output);
